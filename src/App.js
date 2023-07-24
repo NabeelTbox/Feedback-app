303f052bcde5461ddb0c1608b568e33b093ab281
Nabeel <Nabeel.Tbox@gmail.com>
2023-07-24 21:29:44 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
+import Header from "./components/Header";
+import FeedbackItems from "./components/FeedbackItems";
+
 function App() {
-  const title = "Blog Post";
-  const body = "My blog Post";
-  const comments = [
-    { id: 1, Text: "Comment one" },
-    { id: 2, Text: "Comment two" },
-    { id: 3, Text: "Comment Three" },
-  ];
-
-  const loading= false
-  const showComments= true
-
-  if(loading){
-    return <h1>Loading....</h1>
-  }
-
   return (
-    <div>
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-
-      {showComments ? (      <div className="comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.Text}</li>
-          ))}
-        </ul>
-      </div>) : 'no'}
-
-
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackItems />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+function Header({text}) {
+  return (
+    <header style={{backgroundColor: 'black', color:'red'}}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  )
+}
+
+Header.defaultProps={
+    text: 'Feedback UI',
+
+};
+
+
+
+export default Header
+
+
